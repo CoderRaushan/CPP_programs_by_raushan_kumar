a14f844882497e84191415ebfbf9fd8d450b2fab
Raushan <raushankumar23082004@gmail.com>
2023-10-08 00:55:57 +0530

    u

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,25 +1,5 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int i, j, n;
-    cout << "enter a number:";
-    cin >> n;
-    for (i = 1; i <= n; i++)
-    {
-        for (j = 1; j <=n; j++)
-        {
-            if (j <=i)
-            {
-                cout << "*";
-            }
-            else
-            {
-                cout << " ";
-            }
-        }
-        cout << "\n";
-    }
 
-    return 0;
-}
+            if(j>=i)
+            cout<<"*";
+            else
+            cout<<" ";
