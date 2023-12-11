bfe2753a5d88400e23cabb4d2c406879eca237ad
Raushan <raushankumar23082004@gmail.com>
2023-12-11 10:56:59 +0530

    udated

---
+++ table_print.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter how many times your have to print numbers:"<<endl;
+cin>>n;
+ for (int i = 1; i <=n; i++)
+ {
+   for (int j = 1; j<=10;j++)
+   {
+    cout<<i*j<<" ";
+   }
+   cout<<endl;
+ }
+ 
+return 0;
+}
