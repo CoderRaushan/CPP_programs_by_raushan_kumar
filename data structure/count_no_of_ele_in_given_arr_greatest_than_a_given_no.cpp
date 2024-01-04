1b195c9d15fdb74b1c68d9f007a79e8c1679b9fa
Raushan <raushankumar23082004@gmail.com>
2024-01-04 21:49:58 +0530

    added

--- data structure/count_no_of_ele_in_given_arr_greatest_than_a_given_no.cpp
+++ data structure/count_no_of_ele_in_given_arr_greatest_than_a_given_no.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
- int arr[100],n,count=0;
+ int arr[100],n,x,count=0;
 cout<<"enter size of array:"<<endl;
 cin>>n;
 cout<<"enter elements of array:"<<endl;
@@ -10,10 +10,15 @@ for (int i = 0; i <n; i++)
 {
    cin>>arr[i];
 }
+cout<<"enter your target element"<<endl;
+cin>>x;
 for (int i = 0; i < n; i++)
 {
-  
+  if(arr[i] > x)
+  {
+   count++;
+  }
 }
-
+cout<<count;
 return 0;
 }
