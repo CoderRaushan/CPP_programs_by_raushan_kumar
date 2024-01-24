6035c3091f98cd76d57db3cf732a61dddb443a14
Raushan <raushankumar23082004@gmail.com>
2024-01-24 21:42:50 +0530

    up

--- Array/pascal_traiangle.cpp
+++ Array/pascal_traiangle.cpp
@@ -1,33 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 vector<vector<int>> pascal(int n)
 {
-   vector<vector<int>>v;
-   for (int i = 0; i < n; i++)
-   {
-    vector<int>a(i);
-    v.push_back(a);
-   }
-   for (int i = 0; i <n; i++)
-   {
-    for (int j=i; j<n; j++)
+
+    vector<vector<int>> v;
+
+    for (int i = 0; i < n; i++)
     {
-      if(j==0 || j==i)
-      {
-        v[i][j]=1;
-      }
-      else
-      {
-        v[i][j]=v[i-1][j]+v[i-1][j-1];
-      }
+        vector<int> a(i + 1);
+        v.push_back(a);
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0 || j == i)
+            {
+                v[i][j] = 1;
+            }
+            else
+            {
+                v[i][j] = v[i - 1][j] + v[i - 1][j - 1];
+            }
+        }
     }
+
+    // Move the return statement outside the loop
     return v;
-   }
-   
-    
 }
 int main()
 {
- 
-return 0;
+    int N;
+    cout << "Enter the number of rows for Pascal's Triangle: ";
+    cin >> N;
+
+    vector<vector<int>> pascalsTriangle = pascal(N);
+
+    // Display Pascal's Triangle
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << pascalsTriangle[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
