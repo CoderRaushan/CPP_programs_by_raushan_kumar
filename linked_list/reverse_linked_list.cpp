41f76d4d001150cc6c5e5f5f981d116c369646a1
Raushan <raushankumar23082004@gmail.com>
2024-01-24 17:24:24 +0530

    up

---
+++ linked_list/reverse_linked_list.cpp
@@ -0,0 +1,13 @@
+// Node* reverseLinkedList(Node *head)
+// {
+//       Node *temp=head;
+//       Node *prev=NULL;
+//    while(temp!=NULL)
+//    {    Node*front=temp->next;
+//         temp->next=prev;
+//         prev=temp;
+//         temp=front;
+//    }
+//    return prev;
+        
+// }
