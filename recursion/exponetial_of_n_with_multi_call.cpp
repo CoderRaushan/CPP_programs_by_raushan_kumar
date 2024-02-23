7fd8b860f923a7a4e0f33e4d59ffa898bdd8e0f5
Raushan <raushankumar23082004@gmail.com>
2024-02-23 22:54:17 +0530

    rec

---
+++ recursion/exponetial_of_n_with_multi_call.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int pow(int i,int n)
+{
+if(n==1) return i ;
+int ans=pow(i,n/2);
+if(n%2==0) return ans*ans;
+else return i*ans*ans; 
+}
+int main()
+{
+cout<<pow(2,8);
+return 0;
+}
