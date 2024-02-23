31e005d3d5973d134ce29c772eb115e583f8b181
Raushan <raushankumar23082004@gmail.com>
2024-02-23 23:17:40 +0530

    rec

---
+++ recursion/stair_path.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int stair(int n)
+{
+if(n==1) return 1;
+if(n==2) return 2;
+return stair(n-1)+stair(n-2);
+}
+int main()
+{
+cout<<stair(5);
+return 0;
+}
