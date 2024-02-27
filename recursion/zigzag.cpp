d48aa8a6fdf31e9278eaab6d80a893c9e777008a
Raushan <raushankumar23082004@gmail.com>
2024-02-27 18:10:58 +0530

    recurion

--- recursion/zigzag.cpp
+++ recursion/zigzag.cpp
@@ -1,7 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+void zigzag(int n)
+{
+if(n==0)return;
+cout<<n;
+zigzag(n-1);
+cout<<n;
+zigzag(n-1);
+cout<<n;
+}
 int main()
 {
- 
+ zigzag(4);
 return 0;
 }
