b81e56341bd2f4835522a50e8a7cdc1293ca2781
Raushan <raushankumar23082004@gmail.com>
2024-03-09 21:47:36 +0530

    rec

---
+++ recursion/combinationSum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void combibation(vector<int> v, int arr[], int target, int n, int idx)
+{
+    if (target == 0)
+    {
+        for (int i = 0; i < v.size(); i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+    if (target < 0)
+        return;
+    for (int i = idx; i <n; i++)
+    {
+        v.push_back(arr[i]);
+        combibation(v, arr, target - arr[i], n, idx);
+        v.pop_back();
+    }
+}
+int main()
+{
+    int arr[] = {2, 3, 5};
+    vector<int> v;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 8;
+    combibation(v, arr, target, n, 0);
+    return 0;
+}
