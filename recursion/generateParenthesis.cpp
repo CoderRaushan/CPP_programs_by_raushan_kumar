eacf31b570ecd526de37d2b0c615f0479b4a94a5
Raushan <raushankumar23082004@gmail.com>
2024-03-09 23:13:16 +0530

    rec

---
+++ recursion/generateParenthesis.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void generate(string s,int open,int close,int n)
+{
+if(close==n)
+{
+    cout<<s<<endl;
+    return ;
+}
+if(open < n)generate(s+'(',open+1,close,n);
+if(close<open)generate(s+')',open,close+1,n);
+}
+int main()
+{
+generate("",0,0,3);
+return 0;
+}
