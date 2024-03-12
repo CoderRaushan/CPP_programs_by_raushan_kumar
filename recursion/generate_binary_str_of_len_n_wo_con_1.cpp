e6465b6df49a855ad1f113d40b4fc1455a6f6dc3
Raushan <raushankumar23082004@gmail.com>
2024-03-12 17:26:24 +0530

    rec

--- recursion/generate_binary_str_of_len_n_wo_con_1.cpp
+++ recursion/generate_binary_str_of_len_n_wo_con_1.cpp
@@ -1,21 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void binary(string str,int n)
+void binary(string str, int n)
 {
-if(str.length()==n)
-{
- cout<<str<<endl;
- return;
-}
-binary(str+"0",n);
-if(str=="" ||  str[str.length()-1]=='0') 
-{
-    binary(str+"1",n);
-}
+    if (str.length() == n)
+    {
+        cout << str << endl;
+        return;
+    }
+    binary(str + "0", n);
+    if (str == "" || str[str.length() - 1] == '0')
+    {
+        binary(str + "1", n);
+    }
 }
 int main()
 {
-int n=3;
-binary("",n);
-return 0;
+    int n = 4;
+    binary("", n);
+    return 0;
 }
