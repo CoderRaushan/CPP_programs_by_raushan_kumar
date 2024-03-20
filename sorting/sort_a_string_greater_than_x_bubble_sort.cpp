51f1cffa2b995aa11db8e478b04a36efb31c8e09
Raushan <raushankumar23082004@gmail.com>
2024-03-20 18:36:22 +0530

    sorting

---
+++ sorting/sort_a_string_greater_than_x_bubble_sort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ string s="AZYZXBDJKX";
+ string str;
+ for (int i = 0; i <s.length(); i++)
+ {
+       if(s[i]>='X') str.push_back(s[i]);
+ }
+ cout<<str<<endl;
+ bool flag=true;
+ for (int i = 0; i <str.length()-1; i++)
+ {
+    for (int j = 0; j <str.length()-1; j++)
+    {
+       if(str[j]<str[j+1])
+       {
+        swap(str[j],str[j+1]);
+        flag=false;
+       }
+    }
+    if(flag==true)break;
+ }
+  cout<<str;
+return 0;
+}
