7d1fec8b0b86ac62ac1b037eed1c79427329d472
Raushan <raushankumar23082004@gmail.com>
2024-04-02 21:40:44 +0530

    bs

---
+++ Data Structure/Binary Search/firstOccurrence.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+void binaryseach(vector<int>&nums,int target)
+{   bool flag=false;
+    int n=nums.size();
+    int low=0;
+    int high=n-1;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target) 
+        {
+           if(nums[mid-1]==target)
+           {
+              high=mid-1;
+           }
+           else 
+           {
+            cout<<mid;
+            flag=true; 
+            break;
+           }
+        }
+        else if(nums[mid]<target) low=mid+1;
+        else high=mid-1;
+    }
+    if(!flag)cout<<"Not Found";
+}
+int main()
+{
+ vector<int>nums{1,3,4,5,4,4,4,4,4,4,5,6,7,8,9};
+ int x=45;
+ binaryseach(nums,x);
+return 0;
+}
