a9056f93a8798ea86363498af3ef2167325be3bf
Raushan <raushankumar23082004@gmail.com>
2024-04-02 21:11:21 +0530

    bs

---
+++ Data Structure/Binary Search/lower_bound.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void binaryseach(vector<int>&nums,int target)
+{   bool flag=false;
+    int n=nums.size();
+    int low=0;
+    int high=n-1;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target) 
+        {
+            flag=true;
+            cout<<nums[mid-1];
+            break;
+        }
+        else if(nums[mid]<target) low=mid+1;
+        else high=mid-1;
+    }
+    if(flag==false) cout<<nums[high];
+}
+int main()
+{
+ vector<int>nums{1,3,4,5,6,7,8,9};
+ int x=5;
+ binaryseach(nums,x);
+return 0;
+}
