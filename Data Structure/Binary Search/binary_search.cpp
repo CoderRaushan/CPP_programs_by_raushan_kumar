0cbec561b3c1421aedef3d56c2741a5b508abcaa
Raushan <raushankumar23082004@gmail.com>
2024-04-02 20:04:13 +0530

    bs

---
+++ Data Structure/Binary Search/binary_search.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binaryseach(vector<int>&nums,int target)
+{
+     int n=nums.size();
+    int low=0;
+    int high=n-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(nums[mid]==target) return mid ;
+        else if(nums[mid]<target) low=mid+1;
+        else high=mid-1;
+    }
+    return -1;
+}
+int main()
+{
+ vector<int>nums{1,3,4,5,6,7,8,9};
+ int target=68;
+ cout<<binaryseach(nums,target);
+return 0;
+}
