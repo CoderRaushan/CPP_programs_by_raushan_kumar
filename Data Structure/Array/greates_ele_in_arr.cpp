ebcd4d2e6162b0e7132f044685f1dc343a7dc506
Raushan <raushankumar23082004@gmail.com>
2024-06-10 11:44:49 +0530

    update

--- Data Structure/Array/greates_ele_in_arr.cpp
+++ Data Structure/Array/greates_ele_in_arr.cpp
@@ -1,25 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
- int max=INT_MIN,arr[100],n,i;
-cout<<"enter size of array:"<<endl;
-cin>>n;
-cout<<"enter elements of array:"<<endl;
-for ( i = 1; i <=n; i++)
-{
-   cin>>arr[i];
-}
-cout<<"greater element in an array::"<<endl;
-max=arr[1];
-for ( i = 2; i <=n; i++)//{76,66,39,89,99,42}
-{
-   
-    if(max < arr[i])
+  int max = INT_MIN, arr[100], n, i;
+  cout << "enter size of array:" << endl;
+  cin >> n;
+  cout << "enter elements of array:" << endl;
+  for (i = 0; i <n; i++)
+  {
+    cin >> arr[i];
+  }
+  cout << "greater element in an array:" << endl;
+  // max = arr[1];
+  for (i = 0; i < n; i++) //{76,66,39,89,99,42}
+  {
+    if (max < arr[i])
     {
-      max=arr[i];
+      max = arr[i];
     }
-} 
-cout<<max<<endl; 
-return 0;
+  }
+  cout << max << endl;
+  return 0;
 }
