e673d0c2d7b004e668217dcce094fc3b9579578d
Raushan <raushankumar23082004@gmail.com>
2024-06-12 22:51:47 +0530

    stack

---
+++ Data Structure/Stack/previousGreaterElement.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+vector<int> nextSmallerElement(vector<int> &arr, int n) {
+    vector<int> ans(n);
+    stack<int> st;
+    ans[0] = -1;
+    st.push(arr[0]);
+    for (int i = 1; i < n; i++) {
+        while (!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
+        if (st.empty())
+            ans[i] = -1;
+        else
+            ans[i] = st.top();
+        st.push(arr[i]);
+    }
+    return ans;
+}
+
+int main() {
+    // Example usage
+    vector<int> arr = {4, 5, 2, 10, 8};
+    int n = arr.size();
+    vector<int> result = nextSmallerElement(arr, n);
+    cout << "Next smaller elements are: ";
+    for (int i = 0; i < n; i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
