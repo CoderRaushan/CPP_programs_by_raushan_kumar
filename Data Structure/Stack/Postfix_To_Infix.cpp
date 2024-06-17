b23a144328852fc716facf2383c8ece0f05a8bfc
Raushan <raushankumar23082004@gmail.com>
2024-06-17 12:07:43 +0530

    stack

---
+++ Data Structure/Stack/Postfix_To_Infix.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+    int priority(char c)
+    {
+        if(c=='/' || c=='*') return 2;
+        else return 1;
+    }
+    string calculate(string val1,string val2,char ch)
+    {
+        return val1+ch+val2;
+    }
+int main()
+{
+    stack<string> val;
+    string s = "79+4*8/3-";
+    for (int i = 0; i < s.length(); i++)
+    {
+        // if space occur ingnore it
+        if(s[i]==' ') continue;
+        // check if s[i] is a digit
+        else if (s[i] >= 48 && s[i] <= 57)
+        {
+            val.push(to_string(s[i]-48));
+        }
+        // check if s[i] is a operator +,-,*,/
+        else
+        {
+              string val2=val.top();
+              val.pop();
+              string val1=val.top();
+              char ch=s[i];
+              string ans=calculate(val1,val2,ch);
+              val.push(ans);
+        }
+    }
+    cout<<val.top()<<endl;
+    // cout<<(79+4*8/3-);
+    return 0;
+}
